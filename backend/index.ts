601ef4a2f92222e7fb1786cab52c770edab39849
omurbekov-azamat <avgusttin8@gmail.com>
2023-04-10 22:52:09 +0600

    set up index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,28 @@
-console.log('hello world!');
+import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+import usersRouter from './routers/users';
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/users', usersRouter);
+
+const run = async () => {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+run().catch(console.error);

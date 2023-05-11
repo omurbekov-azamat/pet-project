f02dfaaae4f1b0eeceeb842d87b10aefcdeba0b3
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-11 16:35:49 +0600

    created form for create issue;

--- frontend/src/features/issues/components/IssuesPage.tsx
+++ frontend/src/features/issues/components/IssuesPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import TabPanel from '@mui/lab/TabPanel';
-import {TabContext, TabList} from '@mui/lab';
-import {Box, Tab} from '@mui/material';
+import {LoadingButton, TabContext, TabList} from '@mui/lab';
+import {Box, Grid, Tab, TextField, Typography} from '@mui/material';
+import {useAppSelector} from '../../../app/hooks';
+import {selectUser} from '../../users/usersSlice';
 import {Params} from '../../../types';
 
 interface Props {
@@ -9,6 +11,7 @@ interface Props {
 }
 
 const IssuesPage: React.FC<Props> = ({catchParams}) => {
+    const user = useAppSelector(selectUser);
     const [value, setValue] = React.useState(`1`);
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
@@ -22,13 +25,34 @@ const IssuesPage: React.FC<Props> = ({catchParams}) => {
                         <Tab label="Open" value="1"/>
                         <Tab label="In Progress" value="2"/>
                         <Tab label="Closed" value="3"/>
-                        <Tab label="New issue" value="4"/>
+                        <Tab label="New issue" value="4" disabled={user ? 'manager' !== user.role && true : false}/>
                     </TabList>
                 </Box>
                 <TabPanel value='1'>open</TabPanel>
                 <TabPanel value='2'>in progress</TabPanel>
                 <TabPanel value='3'>closed</TabPanel>
-                <TabPanel value='4'>here is issue form</TabPanel>
+                <TabPanel value='4'>
+                    <Box component='form'>
+                        <Typography fontWeight='bolder'>New Issue</Typography>
+                        <Grid container flexDirection='column' spacing={3} mt={1}>
+                            <Grid item xs={12}>
+                                <TextField label='Title (required)' fullWidth={true}/>
+                            </Grid>
+                            <Grid item>
+                                <TextField rows={4} multiline label='Description' fullWidth={true}/>
+                            </Grid>
+                            <Grid item>
+                                <TextField label='Assignee'/>
+                            </Grid>
+                            <Grid item>
+                                <TextField label='Milestone'/>
+                            </Grid>
+                            <Grid item>
+                                <LoadingButton variant='contained'>Create issue</LoadingButton>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </TabPanel>
             </TabContext>
         </Box>
     );

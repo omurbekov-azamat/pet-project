dc22c0b23b3bdec52c6853bbe2e1513e0249dc3b
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-25 18:25:12 +0600

    created component ProjectDevelopers.tsx with all developers in project

---
+++ frontend/src/features/projects/components/ProjectDevelopers.tsx
@@ -0,0 +1,152 @@
+import React, {useEffect, useState} from 'react';
+import {
+    Accordion, AccordionDetails, AccordionSummary, Avatar,
+    Button,
+    Checkbox,
+    FormControlLabel, FormGroup,
+    Grid,
+    Menu,
+    Typography
+} from '@mui/material';
+import {selectAddDevelopersLoading, selectProject} from '../projectsSlice';
+import {selectDevelopers, selectUser} from '../../users/usersSlice';
+import {useAppDispatch, useAppSelector} from '../../../app/hooks';
+import {getProject, toggleDevelopers} from '../projectsThunks';
+import {enqueueSnackbar, SnackbarProvider} from 'notistack';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {getDevelopers} from '../../users/usersThunks';
+import {useParams} from 'react-router-dom';
+import {LoadingButton} from '@mui/lab';
+import {apiURL} from '../../../constants';
+import {Params} from '../../../types';
+
+const ProjectDevelopers = () => {
+    const catchParams: Params = useParams() as { listName: string, managerName: string, projectName: string, id: string, dashboard: string };
+    const dispatch = useAppDispatch();
+    const user = useAppSelector(selectUser);
+    const project = useAppSelector(selectProject);
+    const developers = useAppSelector(selectDevelopers);
+    const loading = useAppSelector(selectAddDevelopersLoading);
+
+    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+    const [chooseDevelopers, setChooseDevelopers] = useState<string[]>([]);
+
+    const handleClickOpenChooseDevelopers = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleCloseChooseDevelopers = () => {
+        setAnchorEl(null);
+    };
+
+    const handleChangeCheckBox = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, checked} = e.target;
+        if (checked) {
+            const result = await developers.find(item => item._id === name);
+            if (result) {
+                setChooseDevelopers(prev => [...prev, result._id]);
+            }
+        } else {
+            setChooseDevelopers(prev => prev.filter(item => item !== name));
+        }
+    };
+
+    const deleteDeveloper = async (id: string) => {
+        await dispatch(toggleDevelopers({id: catchParams.id, deleteDeveloper: id}));
+        await dispatch(getProject(catchParams.id));
+        await dispatch(getDevelopers(catchParams.id));
+    };
+
+    const handleOnClickAddDeveloper = async () => {
+        if (chooseDevelopers.length > 0) {
+            await dispatch(toggleDevelopers({id: catchParams.id, useDevelopers: chooseDevelopers}))
+            await enqueueSnackbar('You have successfully added developers to the project', {variant: 'success'});
+            dispatch(getDevelopers(catchParams.id));
+            dispatch(getProject(catchParams.id));
+            setChooseDevelopers([]);
+        } else {
+            await enqueueSnackbar('You have to choose developer', {variant: 'warning'});
+        }
+    };
+
+    useEffect(() => {
+        if (user && user.role === 'manager' && catchParams.id) {
+            dispatch(getDevelopers(catchParams.id));
+        }
+    }, [dispatch, catchParams.id, user]);
+
+    return (
+        <Grid container flexDirection='row' spacing={4}>
+            <SnackbarProvider/>
+            {user && user.role === 'manager' &&
+                <Grid item xs={12}>
+                    <Grid container alignItems='stretch' justifyContent='space-between' spacing={2}>
+                        <Grid item xs={12} sm={5}>
+                            <Button
+                                onClick={handleClickOpenChooseDevelopers}
+                                sx={{
+                                    color: 'grey',
+                                    height: '39px',
+                                    border: '1px solid lightblue',
+                                    textTransform: 'capitalize',
+                                    fontSize: '18px',
+                                    width: '100%'
+                                }}
+                            >
+                                Choose Developers
+                            </Button>
+                            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)}
+                                  onClose={handleCloseChooseDevelopers}>
+                                <FormGroup sx={{p: 1, width: 250}}>
+                                    {developers && developers.length > 0 ? developers.map((developer) => (
+                                        <FormControlLabel
+                                            key={developer._id}
+                                            control={<Checkbox onChange={handleChangeCheckBox} name={developer._id}/>}
+                                            label={developer.displayName}/>
+                                    )) : (<Typography>no developers</Typography>)}
+                                </FormGroup>
+                            </Menu>
+                        </Grid>
+                        <Grid item>
+                            <LoadingButton
+                                loading={loading}
+                                onClick={handleOnClickAddDeveloper}
+                                variant='contained'
+                            >
+                                Add developers
+                            </LoadingButton>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            }
+            <Grid item xs={12}>
+                <Typography variant='h5'>Developers: </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                {project && project.developers.length > 0 ? project.developers.map(developer => (
+                    <Accordion key={developer._id}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon/>} aria-controls="panel1a-content"
+                                          id="panel1a-header">
+                            <Grid container spacing={2} alignItems='center'>
+                                <Grid item><Avatar alt={developer.displayName}
+                                                   src={developer.avatar && apiURL + '/' + developer.avatar}/></Grid>
+                                <Grid item><Typography>{developer.displayName}</Typography></Grid>
+                            </Grid>
+                        </AccordionSummary>
+                        <AccordionDetails sx={{background: 'WhiteSmoke'}}>
+                            <Typography>Email: {developer.email}</Typography>
+                            <Typography textTransform='capitalize'>Role: {developer.role}</Typography>
+                            <Grid item>
+                                {user && user.role === 'manager' &&
+                                    <LoadingButton onClick={() => deleteDeveloper(developer._id)}>delete</LoadingButton>
+                                }
+                            </Grid>
+                        </AccordionDetails>
+                    </Accordion>
+                )) : <Typography>There is no developers</Typography>}
+            </Grid>
+        </Grid>
+    );
+};
+
+export default ProjectDevelopers;
--- frontend/src/features/projects/components/ProjectInformation.tsx
+++ frontend/src/features/projects/components/ProjectInformation.tsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import {selectAddDevelopersLoading, selectFetchProjectLoading, selectProject} from '../projectsSlice';
-import {getProject, toggleDevelopers} from '../projectsThunks';
+import React, {useEffect} from 'react';
+import {selectFetchProjectLoading, selectProject} from '../projectsSlice';
 import {useAppDispatch, useAppSelector} from '../../../app/hooks';
-import {selectDevelopers, selectUser} from '../../users/usersSlice';
-import {getDevelopers} from '../../users/usersThunks';
-import {Button, Checkbox, FormControlLabel, FormGroup, Grid, Menu, Typography} from '@mui/material';
-import {enqueueSnackbar, SnackbarProvider} from 'notistack';
-import {LoadingButton} from '@mui/lab';
+import {getProject,} from '../projectsThunks';
+import {Box, Tab, Typography} from '@mui/material';
+import {TabContext, TabList} from '@mui/lab';
+import TabPanel from '@mui/lab/TabPanel';
+import ProjectDevelopers from './ProjectDevelopers';
 import {Params} from '../../../types';
 
 interface Props {
@@ -15,123 +14,35 @@ interface Props {
 
 const ProjectInformation: React.FC<Props> = ({catchParams}) => {
     const dispatch = useAppDispatch();
-    const user = useAppSelector(selectUser);
     const projectLoading = useAppSelector(selectFetchProjectLoading);
-    const loading = useAppSelector(selectAddDevelopersLoading);
     const project = useAppSelector(selectProject);
-    const developers = useAppSelector(selectDevelopers);
 
-    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-    const [chooseDevelopers, setChooseDevelopers] = useState<string[]>([]);
+    const [option, setOption] = React.useState('1');
 
-    const handleClickOpenChooseDevelopers = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleCloseChooseDevelopers = () => {
-        setAnchorEl(null);
-    };
-
-    const handleChangeCheckBox = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, checked} = e.target;
-        if (checked) {
-            const result = await developers.find(item => item._id === name);
-            if (result) {
-                setChooseDevelopers(prev => [...prev, result._id]);
-            }
-        } else {
-            setChooseDevelopers(prev => prev.filter(item => item !== name));
-        }
-    };
-
-    const deleteDeveloper = async (id: string) => {
-        await dispatch(toggleDevelopers({id: catchParams.id, deleteDeveloper: id}));
-        await dispatch(getProject(catchParams.id));
-        await dispatch(getDevelopers(catchParams.id));
-    };
-
-    const handleOnClickAddDeveloper = async () => {
-        if (chooseDevelopers.length > 0) {
-            await dispatch(toggleDevelopers({id: catchParams.id, useDevelopers: chooseDevelopers}))
-            await enqueueSnackbar('You have successfully added developers to the project', {variant: 'success'});
-            dispatch(getDevelopers(catchParams.id));
-            dispatch(getProject(catchParams.id));
-            setChooseDevelopers([]);
-        } else {
-            await enqueueSnackbar('You have to choose developer', {variant: 'warning'});
-        }
+    const optionHandleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setOption(newValue);
     };
 
     useEffect(() => {
         dispatch(getProject(catchParams.id));
     }, [dispatch, catchParams.id]);
 
-    useEffect(() => {
-        if (user?.role === 'manager') {
-            dispatch(getDevelopers(catchParams.id));
-        }
-    }, [dispatch, catchParams.id, user]);
-
     return (
-        <>
-            {projectLoading && <Typography>loading...</Typography>}
-            <Typography mb={5}>Description: {project?.description}</Typography>
-            <Grid container flexDirection='row' spacing={4}>
-                {user && user.role === 'manager' &&
-                    <Grid item xs={12}>
-                        <SnackbarProvider/>
-                        <Button
-                            onClick={handleClickOpenChooseDevelopers}
-                            sx={{
-                                color: 'grey',
-                                height: '56px',
-                                border: '1px solid lightgrey',
-                                textTransform: 'capitalize',
-                                fontSize: '18px',
-                            }}
-                        >
-                            Choose Developers
-                        </Button>
-                        <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)}
-                              onClose={handleCloseChooseDevelopers}>
-                            <FormGroup sx={{p: 1, width: 250}}>
-                                {developers && developers.length > 0 ? developers.map((developer) => (
-                                    <FormControlLabel
-                                        key={developer._id}
-                                        control={<Checkbox onChange={handleChangeCheckBox} name={developer._id}/>}
-                                        label={developer.displayName}
-                                    />
-                                )) : (<Typography>no developers</Typography>)}
-                            </FormGroup>
-                        </Menu>
-                        <LoadingButton
-                            loading={loading}
-                            onClick={handleOnClickAddDeveloper}
-                            variant='contained'
-                        >
-                            Add developers
-                        </LoadingButton>
-                    </Grid>
-                }
-                <Grid item xs={12}>
-                    <Typography>Developers: </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    {project && project.developers.length > 0 ? project.developers.map(developer => (
-                        <Grid container spacing={1} flexDirection='row' alignItems='center' key={developer._id}>
-                            <Grid item>
-                                <Typography>{developer.displayName} {developer.email}</Typography>
-                            </Grid>
-                            <Grid item>
-                                {user && user.role === 'manager' &&
-                                    <LoadingButton onClick={() => deleteDeveloper(developer._id)}>delete</LoadingButton>
-                                }
-                            </Grid>
-                        </Grid>
-                    )) : <Typography>There is no developers</Typography>}
-                </Grid>
-            </Grid>
-        </>
+        <Box sx={{width: '100%'}}>
+            <TabContext value={option}>
+                <Box sx={{borderBottom: 1, borderColor: 'divider', mb: 2}}>
+                    <TabList onChange={optionHandleChange} aria-label="lab API tabs example">
+                        <Tab label="Description" value="1"/>
+                        <Tab label="Developers" value="2"/>
+                    </TabList>
+                </Box>
+                <TabPanel value='1'>
+                    {projectLoading && <Typography>loading...</Typography>}
+                    <Typography mb={2}>{project?.description}</Typography>
+                </TabPanel>
+                <TabPanel value='2'><ProjectDevelopers/></TabPanel>
+            </TabContext>
+        </Box>
     );
 };
 

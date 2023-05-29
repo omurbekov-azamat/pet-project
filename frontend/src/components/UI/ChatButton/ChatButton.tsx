375e9c86f240384696d21713615ec7f2625389c8
omurbekov-azamat <avgusttin8@gmail.com>
2023-05-29 20:10:09 +0600

    create chat with form

--- frontend/src/components/UI/ChatButton/ChatButton.tsx
+++ frontend/src/components/UI/ChatButton/ChatButton.tsx
@@ -1,43 +1,53 @@
 import React, {useState} from 'react';
 import {
-    Button, createTheme,
+    Button, Container, createTheme,
     Dialog,
-    DialogActions,
     DialogContent,
-    DialogTitle,
-    styled,
+    Grid,
+    styled, TextField,
     ThemeProvider,
     Typography
 } from '@mui/material';
 
+const MyButton = styled(Button)({
+    position: 'fixed',
+    bottom: 40,
+    right: 40,
+    width: 50,
+    height: 60,
+    padding: 0,
+    borderRadius: '50%',
+    backgroundColor: '#15d466',
+    '&:hover': {
+
+        backgroundColor: '#128c7e',
+    },
+    zIndex: 9999,
+});
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        },
+    },
+});
+
 const ChatButton = () => {
     const [open, setOpen] = useState(false);
-
-    const MyButton = styled(Button)({
-        position: 'fixed',
-        bottom: 40,
-        right: 40,
-        width: 50,
-        height: 60,
-        padding: 0,
-        borderRadius: '50%',
-        backgroundColor: '#15d466',
-        '&:hover': {
-            backgroundColor: '#128c7e',
-        },
-        zIndex: 9999,
-    });
-
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#ffffff',
-            },
-        },
-    });
+    const [state, setState] = useState('');
 
     const handleClick = () => {
         setOpen(true);
+    };
+
+    const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setState(e.target.value);
+    };
+
+    const submitFormHandler = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(state);
     };
 
     return (
@@ -48,13 +58,51 @@ const ChatButton = () => {
                 </MyButton>
             </ThemeProvider>
             <Dialog open={open} onClose={() => setOpen(false)}>
-                <DialogTitle>Chat</DialogTitle>
-                <DialogContent>
-                    <Typography variant="body1">chat is here</Typography>
+                <DialogContent sx={{px: 1, py: 1}}>
+                    <Container sx={{minWidth: '275px'}}>
+                        <Grid container flexDirection='column'>
+                            <Grid item>
+                                <Typography variant='h5' component='div' color='red'>
+                                    Online users:
+                                </Typography>
+                                <Typography>
+                                    user1
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Grid container direction='column' justifyContent='space-around'>
+                                    <Grid item height={380} overflow='auto'>
+                                    </Grid>
+                                    <Grid item>
+                                        <form onSubmit={submitFormHandler}>
+                                            <Grid container direction='row' alignItems='center'>
+                                                <Grid item xs={8}>
+                                                    <TextField
+                                                        value={state}
+                                                        onChange={inputChangeHandler}
+                                                        label='Message'
+                                                        fullWidth={true}
+                                                        required
+                                                    />
+                                                </Grid>
+                                                <Grid item xs={4}>
+                                                    <Button
+                                                        type='submit'
+                                                        color='warning'
+                                                        variant='contained'
+                                                        sx={{height: '55px', width: '100%'}}
+                                                    >
+                                                        Send
+                                                    </Button>
+                                                </Grid>
+                                            </Grid>
+                                        </form>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Container>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setOpen(false)}>close</Button>
-                </DialogActions>
             </Dialog>
         </>
     );
